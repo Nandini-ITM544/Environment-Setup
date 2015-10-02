66cce26303945354f1124508ad6016f3599d1b5c
Nandini Sridharan <nandini9590@gmail.com>
2015-10-01 19:59:11 -0500

    adding the file

--- install-env.sh
+++ install-env.sh
@@ -4,3 +4,6 @@ sudo apt-get update
 sudo apt-get install -y apache2 git 
 touch /tmp/hello.txt
 echo "ITMO-544 CLOUD" > /tmp/hello.txt
+cd /var/www/html
+sudo git clone https://github.com/Nandini-ITM544/Application-Setup.git 
+
